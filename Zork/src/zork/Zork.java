6fcbc9872ec3d62c2fb64b9afe3b07f923ec1db3
Matthew Wong <mwong1@bayviewglen.ca>
2021-04-30 11:03:19 -0400

    second comment change

--- Zork/src/zork/Zork.java
+++ Zork/src/zork/Zork.java
@@ -1,6 +1,6 @@
 public class Zork {
     public static void main(String[] args) {
       Game game = new Game(); //comment
-      game.play(); //second comment
+      game.play(); //second comment comment comment comment
     }
   }
